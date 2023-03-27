6d31869ce4aeb18743311402b1c4e8a131485378
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-27 14:18:37 +0900

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by 山本響 on 2023/03/27.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
