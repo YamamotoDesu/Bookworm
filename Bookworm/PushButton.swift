e011135b232bd632887627ebc484908a02fd70d2
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-27 15:19:11 +0900

    With @State property

---
+++ Bookworm/PushButton.swift
@@ -0,0 +1,33 @@
+//
+//  PushButton.swift
+//  Bookworm
+//
+//  Created by 山本響 on 2023/03/27.
+//
+
+import SwiftUI
+
+struct PushButton: View {
+    let title: String
+    @State var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct PushButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PushButton(title: "title", isOn: false)
+    }
+}
